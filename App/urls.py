f20f780041118b43c60a39e7f9516c3bbc0e8bcf
wabukowabuko <wabukowabuko@localhost.localdomain>
2024-11-26 18:00:07 +0300

    some 2FA modifications

--- App/urls.py
+++ App/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Login, name="login_page"),
     path('signup/', views.SignUp, name="signup_page"),
     path('verification/', views.IDVerification, name="id_verification_page"),
+    path('2FA', views.otherVerification, name="other_verification_ways_page"),
 ]
--- App/views.py
+++ App/views.py
@@ -5,7 +5,12 @@ def Login(request):
     return render(request, "Login.html")
 
 def SignUp(request):
+    # After clicking the button it should submit and go direct to verification page 
     return render(request, "SignUp.html")
 
 def IDVerification(request):
+    # On submitting moves to page 2FA 
     return render(request, "IDVerification.html")
+
+def otherVerification(request):
+    return render(request, "otherVerificationWays.html")
