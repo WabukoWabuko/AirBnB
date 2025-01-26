a6aff804b8708b99ac25aecdc132c2a5d6942c95
WabukoWabuko <basilwabbs@gmail.com>
2025-01-26 18:00:38 +0000

    Removed app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# Created this file just to host to vercel, but it can be deleted
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-# for accessing Django modules and also kufikia settings za Project
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AirBnB.settings")
-app = get_wsgi_application()
