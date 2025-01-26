ea1adce81a373ae2933589adbe6843af1b6f30c2
WabukoWabuko <basilwabbs@gmail.com>
2025-01-26 19:10:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Replace "your_project_name" with your actual project name
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AirBnB.settings")
 app = get_wsgi_application()
