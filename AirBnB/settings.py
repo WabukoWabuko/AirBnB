6bbbc0338d30adb4ff9f50a40ee2233b0f3c9659
WabukoWabuko <basilwabbs@gmail.com>
2025-01-26 18:17:49 +0000

    added whitenoise to installed apps

--- AirBnB/settings.py
+++ AirBnB/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'django_daraja',
     'Admin',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
