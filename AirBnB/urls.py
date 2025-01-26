954bab4ea632da5bb6ad0227458dd810c558546d
WabukoWabuko <basilwabbs@gmail.com>
2025-01-26 17:41:31 +0000

    trying magic

--- AirBnB/urls.py
+++ AirBnB/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
 urlpatterns = [
+    path('check-static/', lambda request: HttpResponse(
+        '\n'.join(os.listdir(settings.STATIC_ROOT)),
+        content_type='text/plain'
+    )),
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
 ]
