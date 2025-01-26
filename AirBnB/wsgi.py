acc68a6d75c3c4692ca0fd2af8e298a08e3feea8
WabukoWabuko <basilwabbs@gmail.com>
2025-01-26 17:58:52 +0000

    Add app handler to wsgi.py

--- AirBnB/wsgi.py
+++ AirBnB/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirBnB.settings')
 
-application = get_wsgi_application()
+# For Vercel to detect
+app = get_wsgi_application()
