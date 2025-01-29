e81f5d76d4f66dca40e08cdb3a3b7649b4b573e9
WabukoWabuko <basilwabbs@gmail.com>
2025-01-29 10:50:05 +0000

    removed vercel requirements and preferences

--- AirBnB/settings.py
+++ AirBnB/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -12,12 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cuua6r$mp+q84$)aq^mohv=h@1=s(n3(2%48lio%bfpw%cbnu2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # False because i am hosting it.
+DEBUG = True  # False because i am hosting it.
 
 # Here we add the hosts to be used.
-ALLOWED_HOSTS = ['salamabnb.vercel.app', 'localhost',
-                 '127.0.0.1']  # removed * coz it want's precision
-
+ALLOWED_HOSTS = ['salamabnb.vercel.app', 'localhost', '127.0.0.1',
+                 '*']  # removed * coz it want's precision
 
 # Application definition
 
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # For hosting in Vercel
+    #'whitenoise.middleware.WhiteNoiseMiddleware',  # For hosting in Vercel
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AirBnB.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -90,30 +87,31 @@ DATABASES = {
     }
 }
 
-
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -126,7 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -138,19 +135,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # from AI IDK what it will do haha
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # we add the CSRF origins that are trusted
 CSRF_TRUSTED_ORIGINS = [
     'https://salamabnb.vercel.app',  # added because of vercel
     'https://3pld4x-8000.csb.app',
+    'https://b52ffe93-7d54-44cc-93a4-240f31240709-00-1z3o8p8tnfub4.worf.replit.dev'
 ]
-
 
 # To authenticate emails instead of user names
 AUTHENTICATION_BACKENDS = [
--- staticfiles/testing/d.js
+++
@@ -1,3 +0,0 @@
-function name(params) {
-    return 0;
-}
